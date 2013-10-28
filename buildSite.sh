eac5d6106debbd3ee770b851be836c243517f376
Hassan Benjamin Shirani <hshirani@zenfactory.org>
2013-10-28 19:56:36 +0000

    - Changes to image files

--- buildSite.sh
+++ buildSite.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get update
 apt-get upgrade
-apt-get install sudo vim apache2 mysql-server drush php5 php5-common php5-cli
+apt-get install sudo vim apache2 mysql-server drush php5 php5-common php5-cli denyhost psad
 update-alternatives --config editor=vim.basic
